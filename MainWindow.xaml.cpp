f9182f176d5910a34e15abd3ca8a6634a87c5242
Matt Gradwohl <mattgr@msn.com>
2022-11-16 21:37:55 -0800

    beginning to draw cells

--- MainWindow.xaml.cpp
+++ MainWindow.xaml.cpp
@@ -7,6 +7,8 @@
 #include "MainWindow.g.cpp"
 #endif
 
+constexpr int cellsperrow = 50;
+
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -30,18 +32,17 @@ namespace winrt::ModernLife::implementation
     void MainWindow::CanvasControl_Draw(CanvasControl  const& sender, CanvasDrawEventArgs const& args)
     {
         winrt::Windows::Foundation::Size huge = sender.Size();
+        float inc = huge.Width / cellsperrow;
 
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < cellsperrow; i++)
         {
-            float inc = i * huge.Width / 100;
-            args.DrawingSession().DrawLine(0, inc, huge.Height, inc, Colors::DarkSlateGray());
+            args.DrawingSession().DrawLine(0, i* inc, huge.Height, i * inc, Colors::DarkSlateGray());
+            args.DrawingSession().DrawLine(i * inc, 0, i * inc, huge.Width, Colors::DarkSlateGray());
         }
 
-        for (int i = 0; i < 100; i++)
-        {
-            float inc = i * huge.Height / 100;
-            args.DrawingSession().DrawLine(inc, 0, inc, huge.Width, Colors::DarkSlateGray());
-        }
+        float w = (huge.Width / cellsperrow) - 2;
+        args.DrawingSession().DrawRectangle(1, 1, w, w, Colors::Red());
+        args.DrawingSession().DrawRoundedRectangle(inc+1, inc+1, w, w, 2, 2, Colors::Blue());
     }
 
 }
