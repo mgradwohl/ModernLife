33a25409fc43a33f3c63f059a4a22d8a942e72e8
Matt Gradwohl <mattgr@msn.com>
2022-11-16 16:51:58 -0800

    Add project files.

---
+++ MainWindow.xaml.cpp
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::ModernLife::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::CanvasControl_Draw(CanvasControl  const& sender, CanvasDrawEventArgs const& args)
+    {
+        winrt::Windows::Foundation::Size huge = sender.Size();
+
+        for (int i = 0; i < 100; i++)
+        {
+            float inc = i * huge.Width / 100;
+            args.DrawingSession().DrawLine(0, inc, huge.Height, inc, Colors::DarkSlateGray());
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            float inc = i * huge.Height / 100;
+            args.DrawingSession().DrawLine(inc, 0, inc, huge.Width, Colors::DarkSlateGray());
+        }
+    }
+
+}
---
+++ MainWindow.xaml.h
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "MainWindow.g.h"
+using namespace winrt;
+using namespace Microsoft::UI;
+using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::Graphics::Canvas::UI::Xaml;
+using namespace Microsoft::Graphics;
+
+namespace winrt::ModernLife::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void CanvasControl_Draw(Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl  const& sender, Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args);
+    };
+}
+
+namespace winrt::ModernLife::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
